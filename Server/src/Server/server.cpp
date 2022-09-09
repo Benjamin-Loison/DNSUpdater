1118fd27874afddb3c182a849707dee411a02fe7
Benjamin Loison <12752145+Benjamin-Loison@users.noreply.github.com>
2022-09-09 11:15:22 +0200

    Update the server to be up-to-date with the instance I used likewise can use this Git

--- Server/src/Server/server.cpp
+++ Server/src/Server/server.cpp
@@ -10,8 +10,8 @@
     #include <fcntl.h> 
     #include <errno.h> 
     #include <arpa/inet.h> 
-	#include <netinet/in.h>
-	#include <netdb.h>
+    #include <netinet/in.h>
+    #include <netdb.h>
     #define SOCKET int 
     #define SOCKADDR_IN struct sockaddr_in 
 #endif 
@@ -44,7 +44,6 @@
 #define BIND_FILE_PATH "/etc/bind/db.lemnoslife.com"
 #define SUB_DNS "secretSubDomain"
 // can concatenate strings in macros ?
-#define BIND_LINE "secretSubDomain                  IN A      "
 #define BIND_RELOAD "service bind9 reload"
 using namespace std; 
 
@@ -56,21 +55,27 @@ SOCKADDR_IN source;
 
 void initializeNetwork() 
 {
-	#ifdef _WIN32 
+    #ifdef _WIN32 
         WSADATA initWin32; 
         WSAStartup(MAKEWORD(2, 2), &initWin32); 
     #endif 
     socketId = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP); // TCP
+    #ifdef __linux__
+        int enable = 1;
+        if(setsockopt(socketId, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int)) == -1)
+            print("setsockopt(SO_REUSEADDR) failed");
+    #endif
     source.sin_family = AF_INET; 
     source.sin_addr.s_addr = INADDR_ANY; 
     source.sin_port = htons(SERVER_PORT); 
     bind(socketId, (struct sockaddr*)&source, sizeof(source)); 
-	
+    
     thread connection(&connectEvent); 
-    connection.join(); 
+    connection.join(); // why using a thread ? 
     closeConnection(); 
 } 
 
+// could update codes (also client with ll version (more stable))
 void connectEvent() 
 {
     print("Server is listening on port " + convertNbToStr<int>(SERVER_PORT)); 
@@ -78,8 +83,11 @@ void connectEvent()
     while(true)
     { 
         error = 99; 
-        while(error != 0) 
-            error = listen(socketId, 1); 
+        while(error != 0)
+        {
+            print("Listen");
+            error = listen(socketId, 1);
+        }
         #ifdef _WIN32 
             int tempo = sizeof(source);
         #elif  __linux__ 
@@ -99,43 +107,44 @@ void connectEvent()
             clients.erase(newSocket);
         clients[newSocket] = make_tuple(true, "0.0.0.0", 0);
         get<IP>(clients[newSocket]) = string(inet_ntoa(source.sin_addr));
-		get<PORT>(clients[newSocket]) = source.sin_port;
-		threads[newSocket] = thread(&receive, newSocket, get<IP>(clients[newSocket]), convertNbToStr(get<PORT>(clients[newSocket])));
+        get<PORT>(clients[newSocket]) = source.sin_port;
+        threads[newSocket] = thread(&receive, newSocket, get<IP>(clients[newSocket]), convertNbToStr(get<PORT>(clients[newSocket])));
     } 
 } 
 
-void receive(SOCKET client, string ip, string port) 
+void receive(SOCKET client, string ip, string port)
 {
     string msg;
-	thread manageMsgs(&manageMsg, &msg, client, ip, port); 
+    thread manageMsgs(&manageMsg, &msg, client, ip, port); 
     manageMsgs.detach();
     char bufferRcv[NETWORK_LENGTH] = {0}; 
     tuple<bool, string, unsigned short> *tuple = &clients[client]; 
     string lastMsg = "";
-	while(get<CONNECTED>(*tuple)) 
+    while(get<CONNECTED>(*tuple)) 
     { 
         bufferRcv[NETWORK_LENGTH] = {0};
-		if(recv(client, bufferRcv, NETWORK_LENGTH, 0) > 0) 
+        if(recv(client, bufferRcv, NETWORK_LENGTH, 0) > 0) 
         {
-			string rcv = bufferRcv;
-			if(rcv.back() == '#')
-			{
-				msg += rcv;
-				memset(bufferRcv, 0, NETWORK_LENGTH);
-			}
-			else if(rcv == lastMsg)
-			{
-				rcv = rcv.substr(0, rcv.length() - rcv.find_last_of("#"));
-				msg += rcv;
-				memset(bufferRcv, 0, NETWORK_LENGTH);
-			}
+            string rcv = bufferRcv;
+            if(rcv.back() == '#')
+            {
+                msg += rcv;
+                memset(bufferRcv, 0, NETWORK_LENGTH);
+            }
+            else if(rcv == lastMsg)
+            {
+                rcv = rcv.substr(0, rcv.length() - rcv.find_last_of("#"));
+                msg += rcv;
+                memset(bufferRcv, 0, NETWORK_LENGTH);
+            }
         }
         else 
         { 
             get<CONNECTED>(*tuple) = false;
-			break;
+            break;
         } 
         memset(bufferRcv, 0, NETWORK_LENGTH);
+        sleep(1); // otherwise 100 % CPU
     } 
     shutdown(client, 2); 
     #ifdef _WIN320 
@@ -144,51 +153,65 @@ void receive(SOCKET client, string ip, string port)
         close(client); 
     #endif 
     print("Disconnected !");
-	debug("Thread for the client " + convertNbToStr<int>(client) + " finished");
+    debug("Thread for the client " + convertNbToStr<int>(client) + " finished");
 } 
+
+string getIPFromLine(string line)
+{
+    vector<string> lineParts = split(line, " ");
+    unsigned short linePartsSize = lineParts.size();
+    return linePartsSize > 0 ? lineParts[linePartsSize - 1] : "";
+}
 
 void manageMsg(string *msg, SOCKET client, string ip, string port) 
 {
-	print(ip + " " + port);	
+    print(ip + " " + port); 
     while(true) 
     { 
         if(*msg != "") 
         {
             string message = *msg;
-			msg->assign("");
-			vector<string> msgs = split(message, "#");
+            msg->assign("");
+            vector<string> msgs = split(message, "#");
             archive("Received from " + ip + "@" + port + ": "/* + message*/); 
-			for(unsigned int j = 0; j < msgs.size(); j++) 
+            for(unsigned int j = 0; j < msgs.size(); j++) 
             {
                 string message = msgs[j];
-				if(message == PASSWORD)
-				{
-					print("Password matched !");
-					vector<string> lines = getFileContent(BIND_FILE_PATH);
-					bool same = false;
-					for(unsigned short linesIndex = 0; linesIndex < lines.size(); linesIndex++)
-					{
-						string line = lines[linesIndex];
-						if(startsWith(line, SUB_DNS))
-						{
-							string newLine = BIND_LINE + ip;
-							if(lines[linesIndex] == newLine)
-								same = true;
-							else
-								lines[linesIndex] = newLine;
-						}
-					}
-					if(!same)
-					{
-						print("Changing IP to: " + ip);
-						writeFile(BIND_FILE_PATH, "w", lines);
-						system(BIND_RELOAD); // need to run this code with the associated permissions
-					}
-					else
-					{
-						print("Not changing IP to: " + ip);
-					}
-				}
+                if(message == PASSWORD)
+                {
+                    print("Password matched !");
+                    vector<string> lines = getFileContent(BIND_FILE_PATH);
+                    string oldIP = "";
+                    for(string line : lines)
+                    {
+                        if(startsWith(line, SUB_DNS))
+                        {
+                            oldIP = getIPFromLine(line);
+                            break;
+                        }
+                    }
+                    bool same = ip == oldIP;
+                    if(!same)
+                    {
+                        for(unsigned short linesIndex = 0; linesIndex < lines.size(); linesIndex++)
+                        {
+                            string line = lines[linesIndex];
+                            if(getIPFromLine(line) == oldIP)
+                            {
+                                string newLine = replace(line, oldIP, ip);
+                                if(lines[linesIndex] != newLine)
+                                    lines[linesIndex] = newLine;
+                            }
+                        }
+                        print("Changing IP from " + oldIP + " to: " + ip);
+                        writeFile(BIND_FILE_PATH, "w", lines);
+                        system(BIND_RELOAD); // need to run this code with the associated permissions
+                    }
+                    else
+                    {
+                        print("Not changing IP (same) to: " + ip);
+                    }
+                }
             } 
         } 
         else if(get<CONNECTED>(clients[client]) == false) 
@@ -198,10 +221,10 @@ void manageMsg(string *msg, SOCKET client, string ip, string port)
 
 SOCKET getSocketFromIpPort(string ipv4, unsigned short port)
 {
-	for(map<SOCKET, tuple<bool, string, unsigned short>>::iterator it = clients.begin(); it != clients.end(); it++)
-		if(get<IP>(it->second) == ipv4 && get<PORT>(it->second) == port)
-			return it->first;
-	return -1;
+    for(map<SOCKET, tuple<bool, string, unsigned short>>::iterator it = clients.begin(); it != clients.end(); it++)
+        if(get<IP>(it->second) == ipv4 && get<PORT>(it->second) == port)
+            return it->first;
+    return -1;
 }
 
 void closeConnection() 
